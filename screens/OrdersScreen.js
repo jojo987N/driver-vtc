7f612126730ea3024d273501d63a5a0662553c9b
nass <cabliveer@gmail.com>
2022-10-04 18:47:03 +0200

    d

--- screens/OrdersScreen.js
+++ screens/OrdersScreen.js
@@ -261,7 +261,7 @@ const Earnings = () => {
         <Text style={{
           color: "#1a8cff"
         }}>{currency}</Text> 0.00 </Text> */}
-      <View style={{ flexDirection: "row" }}>
+      <View style={{ flexDirection: "row", justifyContent: "space-around"}}>
         <Text style={{ fontSize: 20, color: "white" }}>Votre crédit: </Text>
         <Text style={{ color: "#1a8cff", fontSize: 25 }}>{currency}</Text>
         <Text style={{ fontSize: 25, color: "white" }}>0.00</Text>

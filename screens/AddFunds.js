b8f7d707521614350c7d858f65379865d47d464b
nass <cabliveer@gmail.com>
2022-10-05 16:15:00 +0200

    dg

--- screens/AddFunds.js
+++ screens/AddFunds.js
@@ -1,90 +1,113 @@
-import { View, Text, StyleSheet, TouchableOpacity, StatusBar} from 'react-native'
-import React, { useState } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar,
+} from "react-native";
+import React, { useState } from "react";
 // import { ArrowBack } from '../components/restaurantDetail/About'
-import { CheckBox, Divider, } from 'react-native-elements'
-import { stripePayment } from '../utils'
-import { useStripe } from '@stripe/stripe-react-native';
+import { CheckBox, Divider } from "react-native-elements";
+import { stripePayment } from "../utils";
+import { useStripe } from "@stripe/stripe-react-native";
 
+export default function AddFunds({ navigation, setModalVisible, setAmount }) {
+  const [amount1, setAmount1] = useState(50);
+  const [amount2, setAmount2] = useState(100);
+  const [amount3, setAmount3] = useState(150);
+  // const [checked1, setChecked1] = useState(false)
+  // const [checked2, setChecked2] = useState(false)
+  // const [checked3, setChecked3] = useState(false)
 
-export default function AddFunds({ navigation, setModalVisible, setAmount}) {
+  const [checked, setChecked] = useState(new Array(3).fill(false));
+  const stripe = useStripe();
+  return (
+    <View style={styles.container}>
+      {/* <ArrowBack navigation={navigation} /> */}
+      <Text style={styles.title}>Add Funds</Text>
+      <Divider
+        style={{ marginHorizontal: 10, marginTop: 20, marginBottom: 20 }}
+      />
 
-    const [amount1, setAmount1] = useState(50)
-    const [amount2, setAmount2] = useState(100)
-    const [amount3, setAmount3] = useState(150)
-    const [checked1, setChecked1] = useState(false)
-    const [checked2, setChecked2] = useState(false)
-    const [checked3, setChecked3] = useState(false)
-    const stripe = useStripe();
-    return (
-        <View style={styles.container}>
-            {/* <ArrowBack navigation={navigation} /> */}
-            <Text style={styles.title}>Add Funds</Text>
-            <Divider style={{ marginHorizontal: 10, marginTop: 20, marginBottom: 20 }} />
+      <View style={{ flex: 1 }}>
+        <CheckBox
+          title={
+            <Text style={{ marginLeft: 10, fontSize: 25 }}>${amount1}</Text>
+          }
+          checked={checked[0]}
+          checkedIcon="dot-circle-o"
+          uncheckedIcon="circle-o"
+          containerStyle={styles.containerStyle}
+          onPress={() => {
+            setChecked([true, false, false])
+          }}
+        />
+        <Divider
+          style={{ marginHorizontal: 20, marginTop: 20, marginBottom: 20 }}
+        />
+        <CheckBox
+          title={
+            <Text style={{ marginLeft: 10, fontSize: 25 }}>${amount2}</Text>
+          }
+          checked={checked[1]}
+          checkedIcon="dot-circle-o"
+          uncheckedIcon="circle-o"
+          containerStyle={styles.containerStyle}
+          onPress={() => {
+            setChecked([false, true, false])
+          }}
+        />
 
-            <View style={{flex: 1}}>
-                <CheckBox
-                    title={<Text style={{ marginLeft: 10, fontSize: 25}}>${amount1}</Text>}
-                    checked={checked1}
-                    checkedIcon="dot-circle-o"
-                    uncheckedIcon="circle-o"
-                    containerStyle={styles.containerStyle} 
-                    onPress={() => {
-                        setChecked1(!checked1)
-                    }}/>
-                <Divider style={{ marginHorizontal: 20, marginTop: 20, marginBottom: 20 }} />
-                <CheckBox
-                    title={<Text style={{ marginLeft: 10, fontSize: 25}}>${amount2}</Text>}
-                    checked={checked2}
-                    checkedIcon="dot-circle-o"
-                    uncheckedIcon="circle-o"
-                    containerStyle={styles.containerStyle} 
-                    onPress={() => {
-                        setChecked2(!checked2)
-                    }}/>
-            
-
-            <Divider style={{ marginHorizontal: 20, marginTop: 20, marginBottom: 20 }} />
-            <CheckBox
-                title={<Text style={{ marginLeft: 10, fontSize: 25}}>${amount3}</Text>}
-                checked={checked3}
-                checkedIcon="dot-circle-o"
-                uncheckedIcon="circle-o"
-                containerStyle={styles.containerStyle} 
-                onPress={() => {
-                    setChecked3(!checked3)
-                }}/>
-            </View>
-            <View style={{marginHorizontal: 20, flexDirection: "row", marginBottom: 20}}>
-                <TouchableOpacity style={{  flex: 1 }}   onPress={()=>{
-                    setModalVisible(!checked)
-                 }}>
-                    <Text style={{padding: 20, fontSize: 20 }}>CANCEL</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={{ backgroundColor: "black", flex: 1 }}
-                 onPress={()=>{
-                    stripePayment(stripe, amount2, setModalVisible, setAmount)
-                 }}
-                >
-                    <Text style={{ color: "white", padding: 20, fontSize: 20 }}>CONFIRM</Text>
-                </TouchableOpacity>
-
-            </View>
-             
-        </View>
-    )
+        <Divider
+          style={{ marginHorizontal: 20, marginTop: 20, marginBottom: 20 }}
+        />
+        <CheckBox
+          title={
+            <Text style={{ marginLeft: 10, fontSize: 25 }}>${amount3}</Text>
+          }
+          checked={checked[2]}
+          checkedIcon="dot-circle-o"
+          uncheckedIcon="circle-o"
+          containerStyle={styles.containerStyle}
+          onPress={() => {
+            setChecked([false, false, true])
+          }}
+        />
+      </View>
+      <View
+        style={{ marginHorizontal: 20, flexDirection: "row", marginBottom: 20 }}
+      >
+        <TouchableOpacity
+          style={{ flex: 1 }}
+          onPress={() => {
+            setModalVisible(!checked);
+          }}
+        >
+          <Text style={{ padding: 20, fontSize: 20 }}>CANCEL</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ backgroundColor: "black", flex: 1 }}
+          onPress={() => {
+            stripePayment(stripe, amount2, setModalVisible, setAmount);
+          }}
+        >
+          <Text style={{ color: "white", padding: 20, fontSize: 20 }}>
+            CONFIRM
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-       flex: 1,
-    },
-    title: {
-        fontSize: 35,
-        marginLeft: 20,
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-
-    },
-    containerStyle:
-        { backgroundColor: "transparent" }
-
-})
+  container: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 35,
+    marginLeft: 20,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  containerStyle: { backgroundColor: "transparent" },
+});

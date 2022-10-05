86d10f014d85e8eeaa0ebbb9a043356442fc1d55
nass <nass@gmail.com>
2022-10-05 10:40:52 +0200

    dg

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -8,24 +8,28 @@ import SignIn from '../screens/authScreens/SignIn';
 import DrawerNavigator from './DrawerNavigator';
 import { UserContext } from '../context/UserContext';
 import SignUp from '../screens/authScreens/SignUp';
+import {StripeProvider} from '@stripe/stripe-react-native'
+
 
 export default function RootNavigation() {
-    const Stack = createStackNavigator();
-    const [userData, setUserData] = useState()
+  const Stack = createStackNavigator();
+  const [userData, setUserData] = useState()
   return (
     <NavigationContainer>
-      <UserContext.Provider  value={{userData, setUserData}}>
-        <Stack.Navigator>
-           
-               <Stack.Screen name="SignIn" component={SignIn} options={{headerShown: false}}/>
-               <Stack.Screen name="SignUp" component={SignUp} options={{headerShown: false}}/>
-              <Stack.Screen name="DrawerNavigator" component={DrawerNavigator} options={{headerShown: false}}/>
-          
-            <Stack.Screen name="OrdersScreen" component={OrdersScreen} options={{headerShown: false}}/>
-            <Stack.Screen name="OrderDelivery" component={OrderDelivery} options={{headerShown: false}}/>
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <StripeProvider publishableKey="pk_test_IvI4y9lJ7FvJR4sPtn1khdkV">
+          <Stack.Navigator>
 
-        </Stack.Navigator>
-        </UserContext.Provider>
+            <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }} />
+            <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
+            <Stack.Screen name="DrawerNavigator" component={DrawerNavigator} options={{ headerShown: false }} />
+
+            <Stack.Screen name="OrdersScreen" component={OrdersScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="OrderDelivery" component={OrderDelivery} options={{ headerShown: false }} />
+
+          </Stack.Navigator>
+        </StripeProvider>
+      </UserContext.Provider>
     </NavigationContainer>
   )
 }

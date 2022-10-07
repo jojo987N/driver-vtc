4e00fdb3d807c8af3b7a9b256539d7852ea49746
nass <cabliveer@gmail.com>
2022-10-07 17:13:36 +0200

    fs

--- components/SettingsComponent.js
+++ components/SettingsComponent.js
@@ -1,39 +1,30 @@
 import { View, Text, SafeAreaView, StatusBar, Image, TextInput, StyleSheet, TouchableOpacity, Pressable } from 'react-native'
 import React, { useState, useEffect, useContext } from 'react'
-import { MaterialIcons } from '@expo/vector-icons'
-import { auth, getDriverInfos } from '../firebase'
+import { Entypo, FontAwesome5, MaterialIcons } from '@expo/vector-icons'
+import { auth, getDriverInfos, updateRestaurantInfos } from '../firebase'
 import { signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { RestaurantContext } from '../context/RestaurantContext'
+import { ScrollView } from 'react-native-gesture-handler'
+import SearchBar from './SearchBar'
 import { UserContext } from '../context/UserContext'
 
 export default function SettingsComponent({ navigation, bs }) {
   const { setUserData, userData } = useContext(UserContext)
-  const [email, setEmail] = useState(userData.email)
-  const [name, setName] = useState(userData.name)
-  const [phone, setPhone] = useState(userData.phone)
-  const [carNumber, setCarNumber] = useState(userData.carNumber)
-  const [carName, setCarName] = useState(userData.Car)
-  const [password, setPassword] = useState('')
-
-  const SignInUser = async () => {
-    const re = await signInWithEmailAndPassword(auth, email, password)
-    getDriverInfos().then(docs => {
-      AsyncStorage.setItem('driverData', JSON.stringify(docs[0]))
-      setUserData(docs[0])
-      navigation.navigate('DrawerNavigator')
-    })
-  }
+  const [email, setEmail] = useState(restaurantData.email)
+  const [name, setName] = useState(restaurantData.name)
+  const [phone, setPhone] = useState(restaurantData.phone)
+  const [address, setAddress] = useState(restaurantData.address)
+  const [city, setCity] = useState(restaurantData.city)
   return (
-    <SafeAreaView style={{
-      flex: 1,
+    <View style={{
+      paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+      backgroundColor: "#e0ebeb",
+      flex: 1
     }}>
       <View style={{
         alignItems: "center",
-        backgroundColor: "blue",
-        borderBottomLeftRadius: 30,
-        borderBottomRightRadius: 30,
-        paddingBottom: 30,
-        paddingTop: 30
+        marginTop: 20
       }}>
         <Pressable onPress={
           () => {
@@ -41,103 +32,68 @@ export default function SettingsComponent({ navigation, bs }) {
           }
         }>
           <Image
-            // source={{ uri: "https://www.shareicon.net/data/128x128/2016/04/10/747353_people_512x512.png" }}
-            source={userData.image?{uri: userData.image}:require("../assets/images/image-profil.png")}
+            source={{ uri: restaurantData.image }}
             style={{
               width: 100,
-              height: 100
+              height: 100,
+              overflow: 'hidden',
+              borderRadius: 100 / 2,
             }}
           />
         </Pressable>
         <Text style={{
-          fontSize: 15, fontWeight: "bold", color: "white",
-          letterSpacing: 2
+          fontSize: 25, fontWeight: "bold", color: "#3d5c5c",
+          letterSpacing: 5
         }}>Upload Image</Text>
       </View>
       <View style={{
-        marginTop: 40,
-        flex: 1,
-        paddingTop: 20
+        marginTop: 40, flex: 1
       }}>
-        <View style={styles.textInputContainer}>
-          <MaterialIcons name="person" size={20} color="#3d5c5c" style={{
-            marginLeft: 6,
-          }} />
-          <TextInput
-            placeholder='Email'
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-            style={styles.textInput} />
+        <View style={{ marginHorizontal: 25 }}>
+          <SearchBar style={{ backgroundColor: "white", borderBottomColor: "grey", borderBottomWidth: 0.3 }}
+            setAddress={setAddress} setCity={setCity}/>
         </View>
-        <View style={styles.textInputContainer}>
-          <MaterialIcons name="person" size={20} color="#3d5c5c" style={{
-            marginLeft: 6,
-          }} />
-          <TextInput
-            placeholder='Email'
-            value={password}
-            onChangeText={(text) => setEmail(text)}
-            style={styles.textInput} />
-        </View>
-        <View style={styles.textInputContainer}>
-          <MaterialIcons name="person" size={20} color="#3d5c5c" style={{
-            marginLeft: 6,
-          }} />
-          <TextInput
-            placeholder='Email'
-            value={name}
-            onChangeText={(text) => setEmail(text)}
-            style={styles.textInput} />
-        </View>
-        <View style={styles.textInputContainer}>
-          <MaterialIcons name="person" size={20} color="#3d5c5c" style={{
-            marginLeft: 6,
-          }} />
-          <TextInput
-            placeholder='Email'
-            value={phone}
-            onChangeText={(text) => setEmail(text)}
-            style={styles.textInput} />
-        </View>
-        <View style={styles.textInputContainer}>
-          <MaterialIcons name="person" size={20} color="#3d5c5c" style={{
-            marginLeft: 6,
-          }} />
-          <TextInput
-            placeholder='Email'
-            value={carName}
-            onChangeText={(text) => setEmail(text)}
-            style={styles.textInput} />
-        </View>
-        <View style={styles.textInputContainer}>
-          <MaterialIcons name="person" size={20} color="#3d5c5c" style={{
-            marginLeft: 6,
-          }} />
-          <TextInput
-            placeholder='Email'
-            value={carNumber}
-            onChangeText={(text) => setEmail(text)}
-            style={styles.textInput} />
-        </View>
-        <TouchableOpacity onPress={() => { }}>
-          <View style={{
-            backgroundColor: "#0080ff",
-            marginHorizontal: 25,
-            borderRadius: 5,
-            marginTop: 30
-          }}>
-            <Text style={{
-              padding: 16,
-              textAlign: "center",
-              color: "white",
-              fontWeight: "bold",
-              fontSize: 15,
-              letterSpacing: 2
-            }}>Update</Text>
+        <ScrollView>
+          <View style={styles.textInputContainer}>
+            <MaterialIcons name="person" size={20} color="#3d5c5c" style={{
+              marginLeft: 6,
+            }} />
+            <TextInput
+              placeholder='Email'
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              style={styles.textInput} />
           </View>
-        </TouchableOpacity>
+          <View style={styles.textInputContainer}>
+            <MaterialIcons name="person" size={20} color="#3d5c5c" style={{
+              marginLeft: 6,
+            }} />
+            <TextInput
+              placeholder='Name'
+              value={name}
+              onChangeText={(text) => setName(text)}
+              style={styles.textInput} />
+          </View>
+          <View style={styles.textInputContainer}>
+            <MaterialIcons name="person" size={20} color="#3d5c5c" style={{
+              marginLeft: 6,
+            }} />
+            <TextInput
+              placeholder='Phone'
+              value={phone}
+              onChangeText={(text) => setPhone(text)}
+              style={styles.textInput} />
+          </View>
+          <TouchableOpacity onPress={() => {
+            updateRestaurantInfos(restaurantData, email, name, phone, address, city, setRestaurantData)
+          }}>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>Update</Text>
+            </View>
+          </TouchableOpacity>
+        </ScrollView>
       </View>
-    </SafeAreaView>
+    </View>
   )
 }
 const styles = StyleSheet.create({
@@ -152,5 +108,19 @@ const styles = StyleSheet.create({
   textInput: {
     width: "90%",
     padding: 10
+  },
+  button: {
+    backgroundColor: "#0080ff",
+    marginHorizontal: 25,
+    borderRadius: 5,
+    marginTop: 30
+  },
+  buttonText: {
+    padding: 16,
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 15,
+    letterSpacing: 2
   }
 })

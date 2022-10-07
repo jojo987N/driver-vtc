4dd35ea54b0a938520d05482134d8b94f385f6c7
nass <cabliveer@gmail.com>
2022-10-07 17:25:49 +0200

    dg

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,143 +1,130 @@
-import { View, Text, SafeAreaView, StatusBar, Pressable, Image, TouchableOpacity} from "react-native";
+import { View, Text, SafeAreaView, StatusBar, Pressable, Image, TouchableOpacity } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import BottomSheet from 'reanimated-bottom-sheet'
 import Animated from "react-native-reanimated";
-import {useEffect, useRef, useState} from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as ImagePicker from "expo-image-picker"
 import * as Permissions from 'expo-permissions'
-import {Camera} from "expo-camera"
-import { updateProduct } from "../firebase"
-import {getDownloadURL, getStorage, ref, uploadBytes} from 'firebase/storage'
+import { Camera } from "expo-camera"
+import { updateProduct, updateRestaurant } from "../firebase"
+import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage'
 import SettingsComponent from "../components/SettingsComponent";
-import { MenuButton } from "./OrdersScreen";
-import { TRANSLATION } from "../global";
+import { RestaurantContext } from "../context/RestaurantContext";
 
-export default function Upload({route, navigation}) {
-    const uploadImage = async (uri)=>{
+export default function Upload({ route, navigation }) {
+  const { restaurantData, setRestaurantData } = useContext(RestaurantContext)
+  const uploadImage = async (uri) => {
     const response = await fetch(uri)
     const blob = await response.blob()
     const storage = getStorage();
-    const storageRef = ref(storage, 'restaurant/');
-    getDownloadURL(storageRef) 
-    .then(url=> updateProduct(product_id,url))
+    const storageRef = ref(storage, uri.substring(uri.lastIndexOf('/') + 1));
+    await uploadBytes(storageRef, blob)
+    const url = await getDownloadURL(storageRef)
+    updateRestaurant(restaurantData.id, url)
   }
-  const [image, setImage] = useState(null) 
-  let openImagePickerAsync = async ()=>{
+  let openImagePickerAsync = async () => {
     let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if(permissionResult.granted === false){
-    alert("Permission to acces camera roll is required")
-    return;
+    if (permissionResult.granted === false) {
+      alert("Permission to acces camera roll is required")
+      return;
     }
     let pickerResult = await ImagePicker.launchImageLibraryAsync()
-   console.log(pickerResult)
-   if(pickerResult.cancelled === true) return;
-  uploadImage(pickerResult.uri)
-   setImage(pickerResult.uri)
+    console.log(pickerResult)
+    if (pickerResult.cancelled === true) return;
+    uploadImage(pickerResult.uri)
+    setRestaurantData({
+      ...restaurantData,
+      image: pickerResult.uri
+    })
   }
-  const renderContent = ()=>(
+  const renderContent = () => (
     <View style={{
       backgroundColor: "white",
       borderTopLeftRadius: 20,
       borderTopRightRadius: 20,
       elevation: 5,
     }}>
-        <View style={{
+      <View style={{
         marginTop: 30
-      }}> 
+      }}>
         <Text style={{
           textAlign: "center",
           fontSize: 20,
           fontWeight: "bold"
-        }}>{TRANSLATION["Upload Photo"]}</Text>
+        }}>Upload Photo</Text>
       </View>
-       <TouchableOpacity  onPress={
-        ()=>openImagePickerAsync()
-      }> 
+      <TouchableOpacity onPress={
+        () => openImagePickerAsync()
+      }>
+        <View style={{
+          marginTop: 30,
+          backgroundColor: "red",
+          marginHorizontal: 20,
+          borderRadius: 10
+        }}>
+          <Text style={{
+            textAlign: "center",
+            fontSize: 15,
+            fontWeight: "bold",
+            padding: 10,
+            color: "white"
+          }}>Take a Photo</Text>
+        </View>
+      </TouchableOpacity>
       <View style={{
-        marginTop: 30,
-        backgroundColor:"red",
+        marginTop: 10,
+        backgroundColor: "red",
         marginHorizontal: 20,
         borderRadius: 10
-      }}> 
+      }}>
         <Text style={{
           textAlign: "center",
           fontSize: 15,
           fontWeight: "bold",
           padding: 10,
           color: "white"
-        }}>{TRANSLATION["Take a Photo"]}</Text>
+        }}>Choose From Library</Text>
       </View>
-      </TouchableOpacity>
-      <TouchableOpacity  onPress={
-        ()=>openImagePickerAsync()
-      }> 
       <View style={{
         marginTop: 10,
-        backgroundColor:"red",
-        marginHorizontal: 20,
-        borderRadius: 10
-      }}> 
-        <Text style={{
-          textAlign: "center",
-          fontSize: 15,
-          fontWeight: "bold",
-          padding: 10,
-          color: "white"
-        }}>{TRANSLATION["Choose From Library"]}</Text>
-      </View>
-      </TouchableOpacity>
-      <TouchableOpacity  onPress={
-        ()=>bs.current.snapTo(2)
-      }> 
-      <View style={{
-        marginTop: 10,
-        backgroundColor:"red",
+        backgroundColor: "red",
         marginHorizontal: 20,
         borderRadius: 10,
         marginBottom: 20
-      }}> 
+      }}>
         <Text style={{
           textAlign: "center",
           fontSize: 15,
           fontWeight: "bold",
           padding: 10,
           color: "white",
-        }}>{TRANSLATION.Cancel}</Text>
+        }}>Cancel</Text>
       </View>
-      </TouchableOpacity>
     </View>
   )
-   const bs = useRef()
-   useEffect(()=>{
-    setTimeout(()=>{
-        bs.current.snapTo(2)
+  const bs = useRef()
+  useEffect(() => {
+    setTimeout(() => {
+      bs.current.snapTo(2)
     }, 2000)
-   }, [])
+  }, [])
   return (
     <GestureHandlerRootView style={{
       backgroundColor: "#eee",
       flex: 1,
       alignItems: "center",
     }}>
-      <BottomSheet 
-      ref={bs}
-      snapPoints={["47%","90%", 0]}
-      renderContent={renderContent}
-            />
+      <BottomSheet
+        ref={bs}
+        snapPoints={["47%", "90%", 0]}
+        renderContent={renderContent}
+      />
       <View style={{
       }}>
-        {image?(<Image source={{uri: image}} 
-        style={{
-          width: 400,
-          height:400,
-          alignSelf: "flex-start",
-          resizeMode: "contain"
-        }}/>):(<></>)}
       </View>
-       <SettingsComponent bs={bs}/>
-       <MenuButton navigation={navigation} />
+     
+      <SettingsComponent bs={bs} />
     </GestureHandlerRootView>
   );
 }
- 

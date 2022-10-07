2083071964b985550ab686571cb5be1ced132926
nass <cabliveer@gmail.com>
2022-10-07 18:07:52 +0200

    df

--- screens/Settings.js
+++ screens/Settings.js
@@ -24,7 +24,7 @@ export default function Upload({ route, navigation }) {
     await uploadBytes(storageRef, blob)
     const url = await getDownloadURL(storageRef)
 
-    console.log("driverId : ", userData.id)
+    console.log("driverId : ", userData.driverId)
     updateRestaurant(restaurantData.id, url)
   }
   let openImagePickerAsync = async () => {

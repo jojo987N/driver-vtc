241954af06d4b65f2670b6d742f463da46dd344a
nass <cabliveer@gmail.com>
2022-10-09 17:55:59 +0200

    dg

--- screens/authScreens/SignIn.js
+++ screens/authScreens/SignIn.js
@@ -24,16 +24,7 @@ export default function SignIn({navigation}) {
     setLoading(true)
     try {
     const re = await signInWithEmailAndPassword(auth, email, password)
-    let datas;
-    const q = query(driversCol, where('id', '==', auth.currentUser?.uid))
-
-    onSnapshot(q, (snapshot) => {
-      datas = snapshot.docs.map((doc) => ({driverId: doc.id, ...doc.data()}) )
-      setUserData({...datas[0], email: re.user.email})
-      // setAmount(snapshot.docs[0].data().wallet?snapshot.docs[0].data().wallet:0)
-      // console.log(datas)
-      
-    });
+     
 
 
 
@@ -56,6 +47,17 @@ export default function SignIn({navigation}) {
    onAuthStateChanged(auth, (user) => {
      //console.log(user)
      if (user) {
+      let datas;
+      const q = query(driversCol, where('id', '==', auth.currentUser?.uid))
+  
+      onSnapshot(q, (snapshot) => {
+        datas = snapshot.docs.map((doc) => ({driverId: doc.id, ...doc.data()}) )
+        setUserData({...datas[0], email: re.user.email})
+        // setAmount(snapshot.docs[0].data().wallet?snapshot.docs[0].data().wallet:0)
+        // console.log(datas)
+        
+      });
+      
       if(userData){
         setLoading(false)
       navigation.navigate('DrawerNavigator')
